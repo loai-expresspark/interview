3d553fc0b103b0d17f28d68b11bbe95ecb9e45fa
loai-expresspark <158069039+loai-expresspark@users.noreply.github.com>
2024-06-03 15:29:35 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+// The data below is an array of objects containing information about books and movies.
+// the titles here consists of the title and the publisher name separated by a hyphen
+// get only the books titles, published After 1990 but the publisher name should be in the output as publisher and not as part of the title
+
+const data = [
+  { title: "The Lord of the Rings - Penguin", type: "book", author: "J.R.R. Tolkien", publicationYear: 1954 },
+  { title: "Harry Potter and the Philosopher's Stone - Atlas books", type: "book", author: "J.K. Rowling", publicationYear: 1997 },
+  { title: "The Hitchhiker's Guide to the Galaxy - Penguin", type: "book", author: "Douglas Adams", publicationYear: 1979 },
+  { title: "The Name of the Wind - Universal", type: "book", author: "Patrick Rothfuss", publicationYear: 2007 },
+  {
+    title: "Harry Potter and the Philosopher's Stone- Warner Bros",
+    type: "movie",
+    author: "Chris Columbus",
+    publicationYear: 2007
+  }
+];
+
+
+
+// example output
+const output = [
+  {
+    title: "xyz",
+    publisher: "abc"
+  }
+]
+
+
+// Write your code here to:
